16c183afd34470afed24d98431c86e4c8039c841
Chen Kasirer <ckasirer@ethz.ch>
2025-12-23 12:06:09 +0100

    upload file types

--- src/components/UploadModel.tsx
+++ src/components/UploadModel.tsx
@@ -78,7 +78,7 @@ export function UploadModel({ apiBaseUrl }: UploadModelProps) {
           ref={fileInputRef} 
           className="hidden-file-input" 
           onChange={onFileChange} 
-          accept=".json" 
+          accept="application/json, .cog"  // TODO: this doesn't show up as proper file type in dialog 
         />
         <div className="upload-icon">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="12" y1="5" x2="12" y2="19"/><line x1="5" y1="12" x2="19" y2="12"/></svg>
